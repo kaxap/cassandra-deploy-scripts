6bcd774fb4a8c8a8e866aaab0fbccf6ed7e2bfbd
kaxap <kashimov@gmail.com>
2019-09-13 05:58:42 +0600

    fix cassandra download url

--- cass-deploy.sh
+++ cass-deploy.sh
@@ -4,6 +4,6 @@
 set -e
 mkdir cassandra-3.11.4
 cd cassandra-3.11.4
-wget http://www.apache.org/dyn/closer.lua/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz
+wget http://apache.website-solution.net/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz
 tar -xzvf apache-cassandra-3.11.4-bin.tar.gz
 bin/cassandra -f
