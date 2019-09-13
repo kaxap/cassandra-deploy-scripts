995417ce37c40882d9be194cb988319953579972
kaxap <kashimov@gmail.com>
2019-09-13 06:36:30 +0600

    add openjdk 1.8

--- cass-deploy.sh
+++ cass-deploy.sh
@@ -2,8 +2,10 @@
 # http://cassandra.apache.org/download/
 
 set -e
+sudo yum install java-1.8.0-openjdk -y
 mkdir cassandra-3.11.4
 cd cassandra-3.11.4
 wget http://apache.website-solution.net/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz
 tar -xzvf apache-cassandra-3.11.4-bin.tar.gz
 bin/cassandra -f
+
